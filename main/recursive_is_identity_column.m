cdbcd83c9776de1e4829ee78d359fa4e000dd8a0
Guilherme Tadashi Maeoka <g117126@g.unicamp.br>
2017-10-22 20:29:46 -0200

    Improvement and properly rename function

--- main/recursive_is_identity_column.m
+++ main/recursive_is_identity_column.m
@@ -1,16 +1,16 @@
  %%
- % 
+ % Recursively verify if V is an identity array.
 %%
-function [r k] = foo_search(V, index, t)
+function [r k] = recursive_is_identity_column(V, index, t)
   i = index;
-  while(i > 0  &&  V(i,1) == 0)
+  while(i > 0  &&  V(i) == 0)
     i = i - 1;
   end
   
   k = i;
   if(i == 0)
     r = t;
-  elseif(V(i,1) ~= 1)
+  elseif(V(i) ~= 1)
     r = false;
   else
     if(t)
