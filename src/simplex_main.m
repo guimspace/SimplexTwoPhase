1702e37227c585344667374be8dab03ffea98853
Guilherme T Maeoka <gui.mspace@gmail.com>
2020-04-08 09:33:27 -0300

    Update URL to repository

--- src/simplex_main.m
+++ src/simplex_main.m
@@ -1,7 +1,7 @@
 %	MIT License
 %
 %	Copyright (c) 2017 Guilherme Tadashi Maeoka
-%	https://github.com/g117126unicamp/SimplexTwoPhase
+%	https://github.com/guimspace/SimplexTwoPhase
 %
 %	Permission is hereby granted, free of charge, to any person obtaining a copy
 %	of this software and associated documentation files (the "Software"), to deal
